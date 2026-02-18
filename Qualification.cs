6ea205c674299a6730cb0599c17fc7666ed3caca
Mathys Chicout <mchic971@gmail.com>
2026-02-18 09:02:17 +0100

    final TP

--- Qualification.cs
+++ Qualification.cs
@@ -1,7 +1,26 @@
-﻿namespace Qualifications
+﻿namespace MesQualifications
 {
     public class Qualification
     {
+        private int code;
+        private string libelle;
+
+        public Qualification(int codeQ, string libelle)
+        {
+            this.code = codeQ;
+            this.libelle = libelle;
+        }
+
+        public int Code
+        {
+            get { return code; }
+            set { code = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"Code: {code}, Libelle: {libelle}";
+        }
 
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Qualifications
+1. Combien de parametres devrait avoir le constructeur ?
+
+Le constructeur devrait avoir 2 paramètres, le code et le libelle.
+
